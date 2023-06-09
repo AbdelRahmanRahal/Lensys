c79e30638e241065ab27aa6ea3a79fa2823c7199
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-06-09 20:36:58 +0300

    Added copy code button with icon

--- src/content.js
+++ src/content.js
@@ -14,43 +14,52 @@ function getSelectedText() {
 function editHighlightedText() {
 	const selectedText = getSelectedText();
 	if (selectedText) {
-		// Creating a new span element to contain everything we edit
-		const newElement = document.createElement('span');
-		newElement.classList.add('lensys-wrapper');
+		// Creating a span element to contain everything we edit
+		const codeBlockWrapper = document.createElement('span');
+		codeBlockWrapper.classList.add('lensys-wrapper');
 		
 		// Applying syntax highlighting to the selected text
 		const codeElement = document.createElement('code');
 		codeElement.textContent = selectedText;
 		hljs.highlightElement(codeElement);
 		
-		// Adding a copy code button
+		// Creating a span element to contain the copy button
 		const copyButtonWrapper = document.createElement('span');
 		copyButtonWrapper.classList.add('lensys-copy-button-wrapper');
+		// Creating  a "copy code" button
 		const copyButton = document.createElement('button');
-		copyButton.classList.add('lensys-copy-button');
 		copyButton.setAttribute('type', 'button');
-		copyButton.textContent = 'Copy Code';
-		copyButtonWrapper.appendChild(copyButton);
+		copyButton.setAttribute('name', 'Copy Code');
+		copyButton.classList.add('lensys-copy-button');
 		copyButton.addEventListener('click', () => {
 			copyCodeToClipboard(codeElement); 
 		});
 
-
-		// Ingulfing the code block in a <pre> element
+		// Creating an img element for the clipboard icon for the copy button
+		const clipboardImg = document.createElement('img');
+		const svgPath = chrome.runtime.getURL('icons/clipboard.svg');
+		clipboardImg.setAttribute('src', svgPath);
+		clipboardImg.setAttribute('alt', 'Copy Code');
+		clipboardImg.classList.add('lensys-copy-button-img')
+		
+		// Appending the img element to the copyButton
+		copyButton.appendChild(clipboardImg);
+		copyButtonWrapper.appendChild(copyButton);
+		// Wrapping the code block in a pre element and finally wrapping everything the original span element
 		const preElement = document.createElement('pre');
 		preElement.appendChild(codeElement);
 		preElement.appendChild(copyButtonWrapper);
-		newElement.appendChild(preElement);
+		codeBlockWrapper.appendChild(preElement);
 
 
-		if (newElement.innerHTML) {
+		if (codeBlockWrapper.innerHTML) {
 			const selection = window.getSelection();
 			if (selection.rangeCount) {
 				const range = selection.getRangeAt(0);
 				// Clearing the selected content and inserting the new span element into the range
 				range.deleteContents();
-				range.insertNode(newElement);
-				range.setStartAfter(newElement);
+				range.insertNode(codeBlockWrapper);
+				range.setStartAfter(codeBlockWrapper);
 				selection.removeAllRanges();
 			}
 		}
